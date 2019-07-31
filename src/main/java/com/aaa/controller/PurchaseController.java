7c2cc154c1aab0338bc7db921c0d920a646b42b3
lizisi123 <lizisiasd??!!310>
2019-07-31 17:59:33 +0800

    申请采购功能

--- src/main/java/com/aaa/controller/PurchaseController.java
+++ src/main/java/com/aaa/controller/PurchaseController.java
@@ -25,26 +25,29 @@ public class PurchaseController {
     @ResponseBody
     public void purchaseGood(@RequestBody JSONObject order){
         Procurement procurement = new Procurement();
+        PurchaseDetail pd=new PurchaseDetail();
         procurement.setProcurementNo("CGDD"+NumberUtil.createNum());
         procurement.setPurchaseTime(new Date());
         procurement.setPurchaser(order.getString("applyperson"));
         procurement.setExplain(order.getString("explain"));
-        if(purchaseService.addProcurement(procurement)){
-            System.out.println("添加采购订单成功");
-        }else{
-            System.out.println("添加采购订单失败");
-        }
+        procurement.setPurchasePrice(order.getDouble("purchasePrice"));
         JSONArray goodArr = order.getJSONArray("goods");
-        PurchaseDetail pd=new PurchaseDetail();
-        pd.setProcurementNo(procurement.getProcurementNo());
+
         for(int i=0;i<goodArr.size();i++){
             JSONObject obj=goodArr.getJSONObject(i);
             pd=(PurchaseDetail)JSONObject.toBean(obj,PurchaseDetail.class);
+            pd.setProcurementNo(procurement.getProcurementNo());
             if(purchaseService.addPurDetail(pd)){
                 System.out.println("添加订单明细成功");
             }else{
                 System.out.println("添加订单明细失败");
             }
+        }
+
+        if(purchaseService.addProcurement(procurement)){
+            System.out.println("添加采购订单成功");
+        }else{
+            System.out.println("添加采购订单失败");
         }
     }
 }
